f2b5a7caa1b8d13aacebf2787206a5a6f1ed413f
AkashKumarRam <heyakashkumarram@gmail.com>
2024-12-10 14:59:02 +0530

    Connected to MongoDB Online Server

--- db.js
+++ db.js
@@ -1,24 +1,29 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const mongoURL = "mongodb://localhost:27017/hotels";
+dotenv.config({
+  path: "./.env",
+});
+
+const mongoURL = process.env.DB_URL;
 
 mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('connected', () => {
-    console.log("Connected to MongoDB Server")
-})
+db.on("connected", () => {
+  console.log("Connected to MongoDB Server");
+});
 
-db.on('error', (err) => {
-    console.log("MongoDB Connection Error =", err)
-})
+db.on("error", (err) => {
+  console.log("MongoDB Connection Error =", err);
+});
 
-db.on('disconnected', () => {
-    console.log("MongoDB disconnected")
-})
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
 
 export default db;
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 import express from "express";
 import db from "./db.js";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
-const app = express();                                                                                                                   
+const app = express();
+
+dotenv.config({
+  path: "./.env",
+});
 
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("Hey this is the world of Akash");
 });
 
-
 import personRoutes from "./routes/personRoutes.js";
-import menuItemRoutes from "./routes/menuItemRoutes.js"
+import menuItemRoutes from "./routes/menuItemRoutes.js";
 
 //Routes
-app.use("/person",personRoutes )
-app.use("/menu", menuItemRoutes)
+app.use("/person", personRoutes);
+app.use("/menu", menuItemRoutes);
 
-
-app.listen(3000, () => console.log("Server listening on Port 3000"));
+app.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
