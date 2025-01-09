0be535fdde337c54cb84756ba4412dc35ad5e5f1
AkashKumarRam <heyakashkumarram@gmail.com>
2025-01-09 17:54:06 +0530

    Implement Passport.js and Bcrypt

--- models/Person.js
+++ models/Person.js
@@ -1,34 +1,73 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const personSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number
-    },
-    work: {
-        type: String,
-        enum: ["chef","waiter","manager"],
-        required: true
-    },
-    mobile: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    address: {
-        type: String
-    },
-    salary: {
-        type: Number,
-        required: true
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  work: {
+    type: String,
+    enum: ["chef", "waiter", "manager"],
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
+
+personSchema.pre("save", async function (next) {
+  const person = this;
+
+  // Hash the  Password only If is Modified or new
+  if (!person.isModified("password")) return next();
+
+  try {
+    // Salt Password Generation
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash Password
+    const hashedPassword = await bcrypt.hash(person.password, salt);
+
+    // Replace the Passport into a Hashed Password
+    person.password = hashedPassword;
+
+    next();
+  } catch (error) {
+    return next(error)
+  }
+});
+
+personSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword,this.password)
+        return isMatch
+    } catch (error) {
+        throw error
     }
-})
-
-export const Person = mongoose.model('Person', personSchema);
+}
+export const Person = mongoose.model("Person", personSchema);
--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 import express from "express";
 import db from "./db.js";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
-
-const app = express();
-
-dotenv.config({
-  path: "./.env",
-});
-
-app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
-
-app.get("/", function (req, res) {
-  res.send("Hey this is the world of Akash");
-});
-
+import passport from "./auth.js"; // Import passport (default export)
+import bodyParser from "body-parser";
 import personRoutes from "./routes/personRoutes.js";
 import menuItemRoutes from "./routes/menuItemRoutes.js";
 
-//Routes
-app.use("/person", personRoutes);
+const app = express();
+
+dotenv.config({ path: "./.env" });
+
+app.use(bodyParser.json()); // Use Express' built-in json parser
+const PORT = process.env.PORT || 3000;
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+  next();
+};
+
+app.use(logRequest);
+
+// Initialize passport middleware
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', { session: false });
+
+app.get("/", localAuthMiddleware, function (req, res) {
+  res.send("Hey this is the world of Akash");
+});
+
+// Routes
+app.use("/person",localAuthMiddleware, personRoutes);
 app.use("/menu", menuItemRoutes);
 
 app.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
